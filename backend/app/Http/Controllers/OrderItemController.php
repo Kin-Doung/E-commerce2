297634ef887adff97e26be4f733ecede18ec5dbd
Kin-Doung <kin.doung@student.passerellesnumeriques.org>
2025-07-02 09:01:02 +0700

    table data

---
+++ backend/app/Http/Controllers/OrderItemController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order_Item;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrderItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Order_Item $order_Item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Order_Item $order_Item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Order_Item $order_Item)
+    {
+        //
+    }
+}
---
+++ backend/app/Http/Controllers/PriviewController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Priview;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PriviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Priview $priview)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Priview $priview)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Priview $priview)
+    {
+        //
+    }
+}
